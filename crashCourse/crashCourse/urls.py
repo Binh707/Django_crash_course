dec7020a1027a5f1874ab2c1e21d9f44d448de51
Binh707 <hoanhbinhqt@gmail.com>
2022-04-24 14:14:53 +0700

    Update urls.py

--- crashCourse/crashCourse/urls.py
+++ crashCourse/crashCourse/urls.py
@@ -24,8 +24,10 @@ def home(request):
 def contact(request):
     return HttpResponse("Contact page")
 
+"""
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',home),
     path('about/',contact),
 ]
+"""
